d7826cdc053fc96f09b110535e514a4c98673116
binayakbishnu <bishnu.binayak12@gmail.com>
2023-09-18 22:52:42 +0530

    fix: minor CLI warnings removed

--- src/Components/HomePage/BasicDetails.jsx
+++ src/Components/HomePage/BasicDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { BsArrowRightCircle } from 'react-icons/bs'
 import { useNavigate } from 'react-router-dom'
 
--- src/Components/HomePage/NewRecipe.jsx
+++ src/Components/HomePage/NewRecipe.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import { BsArrowRightCircle } from 'react-icons/bs'
 
