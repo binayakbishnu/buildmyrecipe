803459130477b44423b0e65d35f9ddf7c5e1d09e
binayakbishnu <bishnu.binayak12@gmail.com>
2023-09-29 19:58:21 +0530

    feat: edited metadata and added logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import logo from '../assets/logo.svg'
+
 function Navbar() {
     return (
-        <div className='flex flex-row justify-center w-full gap-5 m-auto p-5 bg-[rgb(39,52,68)] bg-opacity-80'>
-            <Link to="/home/newrecipe" className='hover:underline'>Home</Link>
-            <Link to="/" className='hover:underline'>About</Link>
+        <div className='relative flex flex-row items-center w-full m-auto p-5 bg-[rgb(39,52,68)] bg-opacity-80'>
+            <img src={logo} alt="logo" className='absolute h-[40px] aspect-auto' />
+            <div className='flex flex-row justify-center items-center flex-1 gap-5'>
+                <Link to="/home/newrecipe" className='hover:underline'>Home</Link>
+                <Link to="/" className='hover:underline'>About</Link>
+            </div>
         </div>
     )
 }
